985538f542f1042fcfdc082e8fdd05ae910475c0
gusta <gusta@DESKTOP-B75S4KE>
2025-01-02 21:02:47 -0300

    Adicionar arquivos de projeto.

---
+++ OakTesteTecnico/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OakTesteTecnico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
